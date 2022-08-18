8042e2dd848ab3f08411c448ed2e7c6c67ddddd8
baifumei <k_katerina_k@icloud.com>
2022-08-18 18:26:08 +0300

    Lecture 1 (modified color of card, add emoji instead of the text)

--- MemorizeGame/ContentView.swift
+++ MemorizeGame/ContentView.swift
@@ -7,12 +7,41 @@
 
 import SwiftUI
 
+let color = RadialGradient(gradient: Gradient(colors: [Color("CardColor"), Color("CardColor2")]), center: .center, startRadius: 20, endRadius: 100)
+let emoji = "🍏"
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+                .fill(color)
+                .frame(width: 90, height: 120)
+                .shadow(color: .black.opacity(0.4), radius: 2)
+            Text(emoji)
+                .font(.title)
+        }
     }
 }
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
