752a373ce5442bfa24bdb580b6748830d555c6fd
baifumei <k_katerina_k@icloud.com>
2022-08-18 14:14:26 +0300

    Initial Commit

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Екатерина К on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
